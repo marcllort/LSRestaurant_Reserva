3b8b4411fc215bb40591eeeb99c4dc88617dfa82
Marc Llort Maulion <mac12llm@gmail.com>
2018-04-04 15:03:15 +0200

    Classe Plat actualitzada

--- src/Model/Comanda.java
+++ src/Model/Comanda.java
@@ -1,22 +1,39 @@
 package Model;
 
+import java.sql.Date;
+import java.util.ArrayList;
 import java.util.LinkedList;
 
 public class Comanda {
 
-    private Plat plat;
+    private String usuari;              //cal enviar el usuari que ha fet la comanda
+    //private Plat plat;
+    private ArrayList<Plat> plats;      //cal que sigui el array dels plats que ha demanat la taula
     private boolean servit;
+    private Date data;                  //  quan fem una comanda cal enviar a la hora que sha fet
 
     /**
      * Constructor sense parametres.
      * Crea un llistat de plats buit.
      */
 
-    public String getNomPlat(){return plat.getNomPlat();}
+    //public String getNomPlat(){return plat.getNomPlat();}
     public boolean esServit(){return servit;}
-    public float getPreuPlat(){return plat.getPreu();}
+    //public float getPreuPlat(){return plat.getPreu();}
 
+    public void addPlat(Plat plat){
+        plats.add(plat);
+    }
 
+    public Plat getPlat(int i){
+        return plats.get(i);
+    }
 
+    public Date getData() {
+        return data;
+    }
 
+    public void setData(Date data) {
+        this.data = data;
+    }
 }
